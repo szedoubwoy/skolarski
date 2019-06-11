c160ca6b612dda0d192d971b11968742abdf7b8b
Szedoubwoy <szedouuu@gmail.com>
2019-06-11 18:26:58 +0200

    commit_2

--- dev-assets/script.js
+++ dev-assets/script.js
@@ -1,18 +1,73 @@
 $(document).ready(function() {
- // $('#recordings').animatescroll();
-
-
+  //navbar scroll
+  $(window).scroll(function(){
+    if($(this).scrollTop() > ( ($('.hero').height() ) - $('.navbar-default').height()-24) ) {
+      $('.navbar-default').css('background', 'rgba(0, 0, 0, 1)');
+    } else {
+      $('.navbar-default').css('background', 'rgba(0, 0, 0, 0.5)');
+    }
+  });
 
   
+  //albums hover effect
+  /*$('#rachmaninov .album-hover-wrapper').hover( function(){
+    $(this).css('opacity', '0.5').fadeOut('medium', 0.8);
+  },
+  function() {
+    $(this).css('opacity', '0').fadeOut('medium', 0.8);
+  });
+
+  $('#rachmaninov .album-hover-wrapper').hover( function(){
+    $(this).css('opacity', '0.5').fadeOut('medium', 0.8);
+  },
+  function() {
+    $(this).css('opacity', '0').fadeOut('medium', 0.8);
+  });
+
+  
+*/
+ // $('#recordings').animatescroll();
+  /*$('.carousel').carousel({
+    interval: false,
+    wrap: false
+  })
+
+  var checkitem = function() {
+    var $this;
+    $this = $(".carousel");
+    if ($(".carousel .carousel-inner .carousel-item:first").hasClass("active")) {
+      $('#prevControl').hide();
+     // $this.children(".right").show();
+    } else if ($("#slideshow .carousel-inner .carousel-item:last").hasClass("active")) {
+      $('#nextControl').hide();
+    } else {
+      $('#prevControl').show();
+      $('#nextControl').show();
+    }
+  };
+  
+  checkitem();
+  
+ */
 
   AOS.init({
     
   })
 
-  $(this).on("click", '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-  });
+//
+  $('.ekko-lightbox .modal-dialog .modal-content .modal-body .ekko-lightbox-nav-overlay').css("color", "red");
+
+  //$(this).on("click", '[data-toggle="lightbox"]', function(event) {
+   // event.preventDefault();
+ //   $(this).ekkoLightbox({
+    
+      
+  //});
+
+
+    ////////////////
+  //});
 
 
 });
+
